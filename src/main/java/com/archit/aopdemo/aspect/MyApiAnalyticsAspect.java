a1624406b9f88964eb6256af96a2865a4f1d7a57
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-29 17:51:36 +0200

    added order (sequence) to advices in different aspects

--- src/main/java/com/archit/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/main/java/com/archit/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.archit.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
 
     @Before("com.archit.aopdemo.aspect.CommonAopPointcutExpressions" +
