8ee6b2eb9d4cdf5057d6c30fa4e9bd96a834337c
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-29 13:52:34 +0200

    pointcut-declarations : reuse pointcuts for different advices

--- src/main/java/com/archit/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/archit/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.archit.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -20,12 +21,23 @@ public class MyDemoLoggingAspect {
      * 3. (..) -> method with 0 or more argument of any type
      */
 
+    // declaring pointcut
+    @Pointcut("execution(* com.archit.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
+
+
     // add all advices for logging
 
     // @Before advice : pointcut expression : match method in a package
-    @Before("execution(* com.archit.aopdemo.dao.*.*(..))")
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>>> executing @Before advice on addAccount" +
                 "()");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n====>>>> performing API analytics");
     }
 }
