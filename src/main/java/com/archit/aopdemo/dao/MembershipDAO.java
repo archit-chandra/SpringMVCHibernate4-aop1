33de46461b2874811e45079e38cf16e157a3a2a2
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-29 11:21:35 +0200

    DAO : added MembershipDAO

---
+++ src/main/java/com/archit/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,12 @@
+package com.archit.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+
+    public void addAccount() {
+        System.out.println(getClass() + " : doing stuff - adding membership " +
+                "account");
+    }
+}
